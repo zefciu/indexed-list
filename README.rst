fb381a07aadfe5f804d1ef9d96bd7b9118cc05c6
Szymon Pyżalski <szymon@pythonista.net>
2013-11-22 20:15:10 +0100

    Prepare for distribution

---
+++ README.rst
@@ -0,0 +1,7 @@
+-------------------------------
+IndexedList and Table
+-------------------------------
+
+IndexedList is a list that has a number of indexes on its items.
+Table is subclass of IndexedList functionally similar to SQL table.
+Documentation soon. Refer to tests for usage examples.
--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='indexed-list',
-    version='0.1',
-    py_modules=['indexed_list']
+    version='0.1.0',
+    author='Szymon Pyżalski',
+    author_email='szymon@pythonista.net',
+    py_modules=['indexed_list'],
+    description='Indexed list and table datatypes',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
